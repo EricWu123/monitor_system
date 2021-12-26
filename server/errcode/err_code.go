83426bbca03d294b705658e37caa40c0f29b0f3c
EricWu123 <630257405@qq.com>
2021-12-26 17:47:16 +0800

    [update] 使用 stringer 自动生成错误码

--- server/errcode/err_code.go
+++ server/errcode/err_code.go
@@ -1,34 +1,11 @@
 package errcode
 
-import "fmt"
-
 type ErrCode int
 
-// 定义错误码
+//go:generate stringer -type ErrCode -linecomment -output code_string.go
 const (
 	ERR_CODE_OK             ErrCode = 0 // 请求成功
 	ERR_CODE_INVALID_PARAMS ErrCode = 1 // 无效参数
 	ERR_CODE_FAILED         ErrCode = 2 // 请求失败
 	ERR_CODE_NO_RIGHTS      ErrCode = 3 // 没有权限
 )
-
-// 定义错误码与描述信息的映射
-var mapErrDesc = map[ErrCode]string{
-	ERR_CODE_OK:             "请求成功",
-	ERR_CODE_INVALID_PARAMS: "无效参数",
-	ERR_CODE_FAILED:         "请求失败",
-	ERR_CODE_NO_RIGHTS:      "没有权限",
-}
-
-// 根据错误码返回描述信息
-func getDescription(errCode ErrCode) string {
-	if desc, exist := mapErrDesc[errCode]; exist {
-		return desc
-	}
-
-	return fmt.Sprintf("error code: %d", errCode)
-}
-
-func (e ErrCode) String() string {
-	return getDescription(e)
-}
--- server/go.mod
+++ server/go.mod
@@ -28,6 +28,9 @@ require (
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
+	golang.org/x/mod v0.5.1 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/tools v0.1.8 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
